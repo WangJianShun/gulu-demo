8b4aad63b23e1be4312be9ddeaae5ff8e38779b9
Wang <1779011185@qq.com>
2018-09-10 22:48:13 +0800

    row和col的雏形完成

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot>123</slot>
+    </div>
+
+</template>
+<script>
+    export default{
+        name:'GuluCol',
+        props:{
+            span:{
+                type:[Number,String]
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .col {
+        height: 100px;
+        width: 50%;
+        background: red;
+        border: 1px solid blue;
+    }
+    $class:col-;
+    @for $n from 1 through 24 {
+        &.#{$class}#{$n} {
+            width:($n/24)*100%
+        }
+    }
+</style>
