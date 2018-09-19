c2812530b841f2a5529e9d9b355a7f4126cfc3d5
Wang <1779011185@qq.com>
2018-09-19 17:30:11 +0800

    监听 popover 的点击事件

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click.stop="xxx">
-        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+    <div class="popover" @click="onclick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
         <span ref="triggerWrapper">
@@ -17,27 +17,48 @@
             }
         },
         methods: {
-            xxx() {
-                this.visible = !this.visible
-                if (this.visible === true) {
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper)
-                        let {width, height, left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
-                        this.$refs.contentWrapper.style.left = left+window.scrollX+'px'
-                        this.$refs.contentWrapper.style.top = top+window.scrollY+'px'
-                        let eventHandler = () => {
-                            this.visible = false
-                            document.removeEventListener('click', eventHandler)
-                            console.log('切换了popover');
-                        }
-                        document.addEventListener('click', eventHandler)
-                    })
-
+            positionContent() {
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {left, top} = this.$refs.triggerWrapper.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            onClickDocument(e) {
+                if (this.$refs.popover &&
+                    (this.$refs.popover === e.target ||
+                        this.$refs.popover.contains(e.target))) {
+                    return
                 }
-            }
-        },
+                this.close()
+            },
+            open() {
+                this.visible = true
+                this.$nextTick(() => {
+                    this.positionContent()
+                    document.addEventListener('click', this.onClickDocument)
 
+                })
+            } ,
+
+            close() {
+                this.visible = false;
+                document.removeEventListener('click', this.onClickDocument)
+                console.log('关闭')
+            },
+
+            onclick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
+                    if (this.visible === true) {
+                        this.close()
+                    } else {
+                        this.open()
+                    }
+                }
+
+            }
+        }
     }
+
 </script>
 <style lang="scss" scoped>
     .popover {
