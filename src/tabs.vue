5853de33c03f77d648fc26ae74e4bd2841feaf3d
Wang <1779011185@qq.com>
2018-09-19 17:42:20 +0800

    简单重构一下代码

--- src/tabs.vue
+++ src/tabs.vue
@@ -36,19 +36,27 @@
                 eventBus: this.eventBus
             }
         },
-        mounted() {
-            if (this.$children.length === 0) {
-                console && console.warn && console.warn('tabs的子组件应该是tabs-body和tabs-head,但是你没有添加此组件')
-            }
-            this.$children.forEach((vm) => {
-                if (vm.$options.name === 'GuluHead') {
-                    vm.$children.forEach((childrenVm) => {
-                        if (childrenVm.$options.name === 'GuluItem' && childrenVm.name === this.selected) {
-                            this.eventBus.$emit(':update:selected', this.selected, childrenVm)
-                        }
-                    })
+        methods: {
+            checkChildren() {
+                if (this.$children.length === 0) {
+                    console && console.warn && console.warn('tabs的子组件应该是tabs-body和tabs-head,但是你没有添加此组件')
                 }
-            })
+            },
+            selectTab() {
+                this.$children.forEach((vm) => {
+                    if (vm.$options.name === 'GuluHead') {
+                        vm.$children.forEach((childrenVm) => {
+                            if (childrenVm.$options.name === 'GuluItem' && childrenVm.name === this.selected) {
+                                this.eventBus.$emit(':update:selected', this.selected, childrenVm)
+                            }
+                        })
+                    }
+                })
+            }
+        },
+        mounted() {
+            this.checkChildren()
+            this.selectTab()
         }
     }
 </script>
