69693bd7d8f9b48a630d6bb7cd07ce07c5862e6f
Wang <1779011185@qq.com>
2018-09-20 21:50:24 +0800

    可以设置 默认selected

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -14,6 +14,10 @@
             title: {
                 default: String,
                 required: true,
+            },
+            name:{
+                type:true,
+                required:true,
             }
         },
         data() {
@@ -27,18 +31,22 @@
                 if (this.open) {
                     this.open = false
                 } else {
-                    this.open = true
-                    this.eventBus && this.eventBus.$emit('update:selected', this)
+                    this.eventBus && this.eventBus.$emit('update:selected', this.name)
                 }
             },
             close() {
                 this.open = false
+            },
+            show(){
+                this.open=true
             }
         },
         mounted() {
-            this.eventBus && this.eventBus.$on('update:selected', (vm) => {
-                if (vm !== this) {
+            this.eventBus && this.eventBus.$on('update:selected', (name) => {
+                if (name!== this.name) {
                     this.close()
+                }else{
+                    this.show()
                 }
             })
         }
--- src/collapse.vue
+++ src/collapse.vue
@@ -13,6 +13,9 @@
             single: {
                 type: Boolean,
                 default: false,
+            },
+            selected:{
+                type:String,
             }
         },
         data() {
@@ -22,12 +25,16 @@
         },
 
         provide() {
-            if (this.single) {
                 return {
                     eventBus: this.eventBus
                 }
-            }
+            },
 
+        mounted(){
+            this.eventBus.$emit('update:selected',this.selected)
+            this.eventBus.$on('update:selected',(name)=>{
+                this.$emit('update:selected',name)
+            })
         }
     }
 </script>
