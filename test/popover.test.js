bb9b33be5fd84d4b07e905c3024597db3c7d9e7e
Wang <1779011185@qq.com>
2018-09-20 17:27:00 +0800

    有个测试用例不知道如何实现  先跳过

--- test/popover.test.js
+++ test/popover.test.js
@@ -10,22 +10,46 @@ describe('Button', () => {
         expect(Popover).to.exist
     })
     it('可以设置position', () => {
-        Vue.component('g-popover',Popover)
-        const div=document.createElement('div')
+        Vue.component('g-popover', Popover)
+        const div = document.createElement('div')
         document.body.appendChild(div)
-        div.innerHTML=`
+        div.innerHTML = `
         <g-popover position="bottom" ref="a"></g-popover>
         <tetmplate slot="content">弹出内容</tetmplate>
         <button>点我</button>
         </g-popover>
         `
-        const vm=new Vue({
-            el:div,
+        const vm = new Vue({
+            el: div,
         })
         vm.$el.querySelector('button').click()
-        vm.$nextTick(()=>{
-            const {contentWrapper}=vm.$refs.a.$refs
+        vm.$nextTick(() => {
+            const {contentWrapper} = vm.$refs.a.$refs
             expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+
+        })
+    })
+    xit('可以设置trigger', (done) => {
+        Vue.component('g-popover', Popover)
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        div.innerHTML = `
+        <g-popover trigger="hover" ref="a"></g-popover>
+        <tetmplate slot="content">弹出内容</tetmplate>
+        <button>点我</button>
+        </g-popover>
+        `
+        const vm = new Vue({
+            el: div,
+        })
+        let event=new Event('mouseenter')
+
+        vm.$el.dispatchEvent(event)
+
+        vm.$nextTick(() => {
+            const {contentWrapper} = vm.$refs.a.$refs
+            expect(contentWrapper).to.exist
+            done()
         })
     })
 
